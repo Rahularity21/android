0dcde36cc7861c51535f4c44ca2247e4b9488019
Rahul Pandey <rprahulpandey21@gmail.com>
2017-03-28 10:43:24 +0530

    Add files via upload
    vaccination start

---
+++ main/java/com/example/rahul/vaccination/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.rahul.vaccination;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.editor_menu, menu);
+        return true;
+    }
+}
